fc34e7fabeee01578f0c5964f52f951c10a41cf8
zoombini_logic <zoombini666@gmail.com>
2026-02-16 09:46:37 -0600

    rebuilding to the docs folder for Git hosting

--- src/main.py
+++ src/main.py
@@ -4,12 +4,17 @@ from inline_markdown import *
 from markdown_blocks import *
 import os
 import shutil
-
+import sys
 
 
 def main():
-    copy_over_dir("./static", "./public")
-    generate_pages_recursive("./content", "./template.html", "./public")
+    if (len(sys.argv)> 1):
+        basepath = sys.argv[1]
+    else:
+        basepath = '/'
+    print(basepath)
+    copy_over_dir("./static", "./docs")
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
 def copy_over_dir(source, destination):
     if (os.path.exists(source) == False):
@@ -32,7 +37,7 @@ def copy_over_dir(source, destination):
         else:
             copy_over_dir(object_path, dest_path)
         
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_file = open(from_path, 'r')
     from_string = from_file.read()
@@ -45,6 +50,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(from_string)
     to_html_string = template_string.replace("{{ Title }}", title)
     to_html_string = to_html_string.replace("{{ Content }}", from_html_string)
+    to_html_string = to_html_string.replace('href="/', f'href="{basepath}')
+    to_html_string = to_html_string.replace('src="/', f'src="{basepath}')
     #print (to_html_string)
     if not os.path.exists(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path))
@@ -52,16 +59,16 @@ def generate_page(from_path, template_path, dest_path):
     dest_file.write(to_html_string)
     dest_file.close()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     object_list = os.listdir(dir_path_content)
     for object in (object_list):
         object_path = dir_path_content + "/" + object
         if (os.path.isfile(object_path) and object[-3:] == ".md"):
             dest_file_name = dest_dir_path + "/" + object.replace(".md", ".html")
-            generate_page(object_path, template_path, dest_file_name)
+            generate_page(object_path, template_path, dest_file_name, basepath)
         if not (os.path.isfile(object_path)):
             new_dest_dir_path = dest_dir_path + "/" + object
-            generate_pages_recursive(object_path, template_path, new_dest_dir_path)
+            generate_pages_recursive(object_path, template_path, new_dest_dir_path, basepath)
 
 
 
